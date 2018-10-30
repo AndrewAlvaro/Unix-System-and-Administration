81c96ec068d27113ac9b0b3ae6b42be11f35000d
Andrew Alvaro <s3559384@student.rmit.edu.au>
2018-10-30 23:31:05 +1100

    S3559384A2

---
+++ scripts/PBR1.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Memory Details:"
+free -h
+echo
+echo "Disk Space:"
+df -mh
+echo
+echo "Network Connection:"
+ifconfig -a
+echo
+echo "Uptime:"
+uptime
+echo
+
+
---
+++ scripts/PBR2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while getopts ":abcde" opt; do
+	case $opt in
+		a)echo "No. of CPU Cores:"
+		       	cat /proc/cpuinfo | awk '/processor/{print $3}' | wc -l 
+			>&2
+		;;
+		b) echo "Current process priority(pid, user, command, nice number, priority):"
+			ps -o pid,user,comm,nice,pri 
+			>&2
+		;;
+		c) echo "Total"
+			ps -u $USER  |
+                        >&2
+                ;;
+		d) echo "h2"
+                        >&2
+                ;;
+		e) echo "h3"
+                        >&2
+                ;;
+
+		\?) echo "Invalid option: -$OPTARG" >&2
+		;;
+	esac
+done
