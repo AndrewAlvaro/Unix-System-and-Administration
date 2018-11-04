0409dd7d9e9099f5969472e40af1537380b75e27
Andrew Alvaro <s3559384@student.rmit.edu.au>
2018-11-04 17:13:11 +1100

    S3559384A2

--- scripts/PBR1.sh
+++ scripts/PBR1.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 echo "----------------------------System Information-------------------------------"
 
+#Calls command 'free -h' to show Memory Details(Memory and Swap)
 echo "Memory Details:"
 free -h
 echo
+
+#Calls command 'df -h'to show Disk Space details in readable human format
 echo "Disk Space:"
-df -mh
+df -h
 echo
+
+#Calls command 'ifconfig -a' to show the status of network interface including interfaces that are down
 echo "Network Connection:"
 ifconfig -a
 echo
+
+#Calls command 'uptime' to show the system running time 
 echo "Uptime:"
 uptime 
 echo "----------------------------------------------------------------------------"
--- scripts/PBR2.sh
+++ scripts/PBR2.sh
@@ -3,22 +3,27 @@
 while getopts ":abcde" opt; do
 	case $opt in
 		a)echo "No. of CPU Cores:"
+			# 'cat /proc/cpuinfo' displays information of system processor | 'awk '/cpu cores/{print $4}'' will only prin				t the cpu cores line, fourth field of the letter from /proc/cpuinfo which displays the total CPU core(s)
 		       	cat /proc/cpuinfo| awk '/cpu cores/{print $4}'
 			>&2
 		;;
 		b) echo "Current process priority(pid, user, command, nice number, priority):"
+			# 'ps -o pid,user,comm,nice,pri' displays the current process running with priority in format PID, user, com				mand, nice, priority	
 			ps -o pid,user,comm,nice,pri 
 			>&2
-		;;
+		;;	
 		c) echo "Total processors running under current user:"
+			# 'ps --user $USER -o pid,user,comm' displays the total processors running under the current user with format 				of pid,user,command and 'wc -l' gives the listing of lines in number(s) equaling to the total of processor			     s running 
 			ps --user $USER -o pid,user,comm | wc -l
                         >&2
                 ;;
 		d) echo "No. of file descripitors owned by current user:"
+			#'lsof -u $USER' displays the number of file descripitors owned by the current user and 'wc -l' gives the lis			       ting of lines in number(s) equaling to the total of file descripitors
 			lsof -u $USER | wc -l
                         >&2
                 ;;
 		e) echo "Maximum default number of file decriptors that can be opened by a process:"
+			#'cat /proc/sys/fs/file-max' displays the maximum default number of file decriptors that can be opened by a p			       rocess
 			cat /proc/sys/fs/file-max
                         >&2
                 ;;
