eac22c4d4c60bae6fee5e80630aa4289ef7d7f61
Andrew Alvaro <s3559384@student.rmit.edu.au>
2018-11-12 21:07:56 +1100

    S3559384A2

--- scripts/PCR1.sh
+++ scripts/PCR1.sh
@@ -1,35 +1,50 @@
 #!/bin/bash
 
-read -p "Enter starting directory of search: " $path
+read -r -p "Enter starting directory of search: "  path1
 
-read -r -p "Enter thing to search for: " typeoftosearch
+
+
+read -r -p "Enter the type of category to search for, please choose path, type, group or fstype: " typeoftosearch
 for typeoftosearch in $typeoftosearch; do
 	if [[ $typeoftosearch == 'path' ]];
 		then
-			$typeoftosearch = path
+			typeoftosearch1="path"
 	elif [[ $typeoftosearch == 'type' ]];
 		then
-			$typeoftosearch = type
+			typeoftosearch1="type"
 	elif [[ $typeoftosearch == 'group' ]];
 		then
-                	$typeoftosearch = group
+                	typeoftosearch1="group"
 	elif [[ $typeoftosearch == 'fstype' ]];
 		then
-                	$typeoftosearch = fstype
+                	typeoftosearch1="fstype"
 	else
-		echo "Please enter path, type, group or fstype to search for only.";
+		echo "Please enter path, type, group or fstype to search for only."
+		
 	fi
 done
-read -r -p "Follow symbolic links? [y/N]" response
-for response in *; do
-case "$response" in 
-	y|Y)
-		$reponse = yes
-	;;
-	n|N)
-		$reponse = no
-	;;
-	*)
+
+
+
+read -r -p "Enter max depth of search? (1-9): " maxdepth
+if ! [[ "$maxdepth" =~ ^[1-9]+$ ]]
+	then 
+		echo "Sorry please select number 1-9 only."
+	fi
+
+
+read -r -p "Follow symbolic links? [y/N] " response
+for response in $response; do
+	if [ $response == 'y' -o  $response == 'Y' ];
+		then
+			response1="-L"
+	elif [ $response == 'n'  -o  $response == 'N' ];
+                then
+			response1=" "	
+	else
 		echo "Please enter Y for Yes and N for No only."
-esac 
+ 	fi
 done
+
+find  $path1 -name -maxdepth $maxdepth $response1
+
