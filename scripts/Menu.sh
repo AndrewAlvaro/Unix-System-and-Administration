05e62e05cc33d6922040902ee5aaad4be1ce8aff
Andrew Alvaro <s3559384@student.rmit.edu.au>
2018-11-13 22:51:58 +1100

    S3559384A2

--- scripts/Menu.sh
+++ scripts/Menu.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+trap "" 2
 echo "==================================================== Menu System ===================================================="
-echo -e "Choose 1: for Find program, Choose 2: for Basic Profiler program, Choose 3: to exit" 
+echo -e "Choose 1: for Find program 
+Choose 2: for Basic Profiler program 
+Choose 3: to exit" 
 read character
+echo "====================================================================================================================="
+
 case $character in 
 	1) ./PCR1.sh
 	;;
 	2) ./PCR2.sh
         ;;
-	3) exit
+	3) echo "Goodbye." 
+		exit
         ;;   
-	*) echo "Please enter the following."
+	*) echo "Please enter the following. Exiting..."
 esac
+
--- scripts/PCR1.sh
+++ scripts/PCR1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "==================================================== Find Program ==================================================="
 
 read -r -p "Enter starting directory of search: "  path1
 
@@ -8,16 +9,28 @@ read -r -p "Enter the type of category to search for, please choose path, type, 
 for typeoftosearch in $typeoftosearch; do
 	if [[ $typeoftosearch == 'path' ]];
 		then
-			typeoftosearch1="-print"
+			typeoftosearch1="-path"
+				read -r -p "Enter path: " pathing
+				pathing2="*$pathing"
 	elif [[ $typeoftosearch == 'type' ]];
 		then
-			typeoftosearch1="-type c"
+			typeoftosearch1="-type"
+				read -r -p "Enter file type:
+				d block
+				c character
+				d directory
+				p named pipe
+				f regular file
+				s socket
+				D door" typing
 	elif [[ $typeoftosearch == 'group' ]];
 		then
-                	typeoftosearch1="-group grp"
+                	typeoftosearch1="-group"
+				read -r -p "Enter group: " grouping
 	elif [[ $typeoftosearch == 'fstype' ]];
 		then
-                	typeoftosearch1="-fstype typ"
+                	typeoftosearch1="-fstype"
+				read -r -p "Enter fstype: " fstyping
 	else
 		echo "Please enter path, type, group or fstype to search for only."
 		
@@ -33,7 +46,7 @@ if ! [[ "$maxdepth" =~ ^[1-9]+$ ]]
 	fi
 
 
-read -r -p "Follow symbolic links? [y/N] " response
+read -r -p "Follow symbolic links? [y/N]: " response
 for response in $response; do
 	if [ $response == 'y' -o  $response == 'Y' ];
 		then
@@ -45,6 +58,7 @@ for response in $response; do
 		echo "Please enter Y for Yes and N for No only."
  	fi
 done
+echo "====================================================================================================================="
 
-find  $response1 $path1 -maxdepth $maxdepth $typeoftosearch1
+find  $response1 $path1 -maxdepth $maxdepth $typeoftosearch1 $pathing2 $typing $grouping $fstyping
 
