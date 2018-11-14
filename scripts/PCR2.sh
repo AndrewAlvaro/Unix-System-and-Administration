fa01f760fcc6520c43f01f4d0de71628156a1cf1
Andrew Alvaro <s3559384@student.rmit.edu.au>
2018-11-15 03:25:51 +1100

    S3559384A2

--- scripts/PCR2.sh
+++ scripts/PCR2.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+echo "=================================================== Basic Profiler =================================================="
 
 read -r -p "Please enter the program name: " programname
 
 for programname in $programname; do
-	if [[  == $programname ]];
+	if [[ $(ps -a | grep $programname | awk '{print $4}') == $programname ]];
                 then	
-			echo $progamname
+			echo $programname
+			if [[$($programname | wc -l) > 1]];
+				then 
+					echo"Please choose program"
+			else
+				echo"Only one program found"
+			fi	
 	else
-		echo nothing choose
+		echo "No program found."
 
 	fi
 done 
