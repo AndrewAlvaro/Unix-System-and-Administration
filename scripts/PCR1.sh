e8dbf71b2b9e5ae855900ffdc2c74cb235df3613
Andrew Alvaro <s3559384@student.rmit.edu.au>
2018-11-21 19:52:40 +1100

    S3559384A2

--- scripts/PCR1.sh
+++ scripts/PCR1.sh
@@ -1,36 +1,56 @@
 #!/bin/bash
 echo "==================================================== Find Program ==================================================="
-
+#prompts the user the path of the starting search
 read -r -p "Enter starting directory of search: "  path1
 
 
-
+#prompts the user to enter the category to search for
 read -r -p "Enter the type of category to search for, please choose path, type, group or fstype: " typeoftosearch
 for typeoftosearch in $typeoftosearch; do
+	
+	#if statement when user selects path will run the following command
 	if [[ $typeoftosearch == 'path' ]];
 		then
+			#makes variable equals to -path
 			typeoftosearch1="-path"
+			#prompts user to enter the path/directory to find
 				read -r -p "Enter path: " pathing
+			#pathing2 variable had to be made as * was needed in the find command to complete the path finding
 				pathing2="*$pathing"
+	
+	#elif statement when user selects type will run the following command			
 	elif [[ $typeoftosearch == 'type' ]];
 		then
+			#make variable equals to -type
 			typeoftosearch1="-type"
+			#prompts user to enter the type of file to find
 				read -r -p "Enter file type:
-				d block
+				b block
 				c character
 				d directory
 				p named pipe
 				f regular file
 				s socket
-				D door" typing
+				D door
+				" typing
+	
+	#elif statement when user selects group will run the following command	
 	elif [[ $typeoftosearch == 'group' ]];
-		then
+		then	
+			#make variable equal to -group
                 	typeoftosearch1="-group"
+			#prompts user to enter the grouping of file to find
 				read -r -p "Enter group: " grouping
+	
+	#elif statement when user selects fstype will the following command
 	elif [[ $typeoftosearch == 'fstype' ]];
-		then
+		then	
+			#make variable equal to -fstype
                 	typeoftosearch1="-fstype"
+			#prompts the user to enter the system file type to find
 				read -r -p "Enter fstype: " fstyping
+	
+	#if none of the above select will print out the following
 	else
 		echo "Please enter path, type, group or fstype to search for only."
 		
@@ -38,14 +58,15 @@ for typeoftosearch in $typeoftosearch; do
 done
 
 
-
+#prompts user to the depth of the search
 read -r -p "Enter max depth of search? (1-9): " maxdepth
+#if statement if the user in put does not equal the following regex will ask the following
 if ! [[ "$maxdepth" =~ ^[1-9]+$ ]]
 	then 
 		echo "Sorry please select number 1-9 only."
 	fi
 
-
+#prompts the user to enter yes or no to follow symbolic links in the search
 read -r -p "Follow symbolic links? [y/N]: " response
 for response in $response; do
 	if [ $response == 'y' -o  $response == 'Y' ];
@@ -59,7 +80,7 @@ for response in $response; do
  	fi
 done
 echo "====================================================================================================================="
-
+#find command is executed with the following variable implemented for the search
 find  $response1 $path1 -maxdepth $maxdepth $typeoftosearch1 $pathing2 $typing $grouping $fstyping
 echo "====================================================================================================================="
 echo "Result finished."
--- scripts/PCR2.sh
+++ scripts/PCR2.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 echo "=================================================== Basic Profiler =================================================="
-
+#prompts user for the programname to basic profile
 read -r -p "Please enter the program name: " programname
 
 for programname in $programname; do
+	#if statement that grabs the programname the user has input from the display of ps and matching it with cmd
 	if [[ $(ps -a | grep $programname | awk '{print $4}') == "$programname" ]];
                 then	
+			#prompts user yes or no to monitor the memory and CPU utilization of the program selected
 			read -r -p "Would you like to monitor the memory and CPU utilization of $programname? [y/N] " response1
 			for response1 in $response1; do
  			       	if [ $response1 == 'y' -o  $response1 == 'Y' ];
                 			then
 						echo "Press 'q' to exit."
+						#grabs the pid of the program by using ps and matching it with cmd
 						pidno=$(ps -a | grep $programname | awk '{print $1}')
+						#executing command top with the pid of the program to monitor the program
 						top -p $pidno
         		       	elif [ $response1 == 'n'  -o  $response1 == 'N' ];
                 			then
